caa80803203ee505da964824ac5ebb7c671e74d4
Gad-Ongoro <gadongoro1@gmail.com>
2023-11-21 11:55:36 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
+greet("GAD")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
 
 def add(num1, num2):
     pass
+    return(num1 + num2)
+sum = add(3, 4)
+print(sum)
 
 def halve(number):
     pass
+    return number/2
